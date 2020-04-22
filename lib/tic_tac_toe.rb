b0ae39f09799c1d97a53b1c4357dff810ecdd00a
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-22 12:47:12 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -30,6 +30,14 @@ def move(board, index, character)
   end
 end
 
+def valid_move?(board, index)
+  # If move is not on board
+  if index > board.length - 1 || index < 0
+    return false
+  end
+  !position_taken?(board, index)
+end
+
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
