e3a45362b5dabc8f7ce344265a13465303b3e994
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-22 12:55:08 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -107,7 +107,7 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   index = input_to_index(gets.strip)
-  valid_move?(board, index) ? move(board, index) : turn(board)
+  valid_move?(board, index) ? move(board, index, current_player(board)) : turn(board)
 end
 board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 turn(board);
