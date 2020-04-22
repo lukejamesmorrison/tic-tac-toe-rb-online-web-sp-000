8df35868d34985e8dbd7bd419f6695647c41dcaf
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-22 12:45:45 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -11,6 +11,25 @@ WIN_COMBINATIONS = [
   [6,4,2]  # Bottom-Left cross
 ]
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def move(board, index, character = "X")
+  if board[index] === " "
+    board[index] = character
+    display_board(board)
+  end
+end
+
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
